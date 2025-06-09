107daf643b07f9e566cf6b3953bc94a61da153e2
Ashik <ashikstenny@gmail.com>
2025-06-10 05:09:12 +0530

    new-blog_2

---
+++ commands.sh
@@ -0,0 +1,13 @@
+# Initialize git if not already done
+git init
+
+# Add all files
+git add .
+
+# Commit your changes
+git commit -m "Initial commit for White Whaling project"
+
+# Add your remote repository
+git remote add origin https://github.com/ashik0204/whitewhale.git
+# Push your code
+git push -u origin main  # or master, depending on your default branch
